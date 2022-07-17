31f1b681d295fbd47d145dc9f756fc82d81837d2
mridul.kumar <mridul.kumar@procol.in>
2022-07-17 23:01:54 +0530

    header done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import Image from 'next/image'
+import HeaderItem from './HeaderItem'
+
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UsersIcon,
+} from '@heroicons/react/outline'
+
+const headerItems = [
+  { title: 'HOME', icon: HomeIcon },
+  { title: 'TRENDING', icon: LightningBoltIcon },
+  { title: 'VERIFIED', icon: BadgeCheckIcon },
+  { title: 'COLLECTIONS', icon: CollectionIcon },
+  { title: 'SEARCH', icon: SearchIcon },
+  { title: 'ACCOUNT', icon: UsersIcon },
+]
 
 function Header() {
   return (
-    <header>
+    <header className="flex flex-col sm:flex-row m-5 justify-between h-auto items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        {headerItems.map((item) => (
+          // eslint-disable-next-line react/jsx-key
+          <HeaderItem title={item.title} Icon={item.icon} />
+        ))}{' '}
+      </div>
       <Image
         className="object-contain"
         src="https://links.papareact.com/ua6"
